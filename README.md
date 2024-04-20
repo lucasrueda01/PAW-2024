740206f47e607d7bcb3bdc637875308a393bf577
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-04-20 08:30:48 -0300

    creando tablas y aplicando modificaciones a tabla plato

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 * git clone <url-repo>
 * cd composer install
 * cp .env.example .env # Editar el .env con los valores deseados
+* Ejecutar migrations: `phinx migrate -e development`
+* Ejecutar nueva migracion: `phinx create migration_name`
 * Ejecutar php -S localhost:8080 -t public/
 
 LICENSE.md
---
+++ db/migrations/20240416164839_primeras_tablas_migration.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class PrimerasTablasMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        
+        $tablePlato = $this->table('plato');
+        $tablePlato->addColumn('nombre_plato', 'string', ['limit' => 60])
+                   ->addColumn('descripcion', 'string', ['null' => true])
+                   ->addIndex('id', ['unique' => true])
+                   ->create();        
+
+    }
+}
