c2841c9d9a08b4c837eadfd230df193d61446136
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-05-03 17:26:51 -0300

    resolviendo ajax  y mapa

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,88 @@
+
+
+function marcarMesas(planoDoc, mesas) {
+    // Iterar sobre las mesas recibidas
+
+    mesas.forEach(function (mesa) {
+        // Obtener el nombre de la mesa
+        var nombreMesa = mesa.nombre;
+
+        // Buscar el elemento de la mesa con el nombre correspondiente y marcarlo con verde
+        var mesaElemento = planoDoc.querySelector(`#${nombreMesa} .mesa`);
+
+        console.log(mesaElemento);
+
+        if (mesaElemento) {
+            mesaElemento.style.fill = "green";
+        }
+    });
+}
+
+function manejarRespuestaAjax(response){
+    console.log(response);
+}
+
+function enviarAjax(params){
+
+        // Configurar la solicitud AJAX
+        var xhr = new XMLHttpRequest();
+        xhr.open(params['method'], params['path'], true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        // Manejar la respuesta del servidor
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                manejarRespuestaAjax(response);
+            }
+        };
+
+        // Convertir el objeto de datos a JSON y enviar la solicitud
+        xhr.send(JSON.stringify(params['datos']));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let planoDelLocal = document.querySelector("#planoDelLocal");
+
+    var planoDoc = planoDelLocal.contentDocument;
+
+    let local = document.querySelector("#local");
+
+    local.addEventListener("change", function () {
+        var localSeleccionado = this.value;
+
+
+        enviarAjax({
+            datos:  {
+                local: localSeleccionado,
+            },
+            method: 'GET',
+            path: 'local/mesas'
+        });
+
+
+    });
+
+    var circulos = planoDoc.querySelectorAll(`[id^="mesa-"], [id^="barra-"]`);
+
+    let mesas = planoDoc.querySelectorAll(".mesa");
+
+    mesas.forEach(function (mesa) {
+        mesa.addEventListener("click", function () {
+            console.log(mesa);
+        });
+    });
+
+    if (circulos) {
+        circulos.forEach(function (circulo) {
+            circulo.addEventListener("click", function () {
+                console.log(circulo);
+            });
+        });
+    }
+
+});
+
 document.addEventListener("DOMContentLoaded", function() {
 
     // Obtener todas las mesas
