a0fc17ceaa443203643d3194eda4ecd198dbcbcf
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-05-20 08:48:30 -0300

    armando part json-ld.view.php

--- src/App/views/parts/head.view.php
+++ src/App/views/parts/head.view.php
@@ -7,33 +7,5 @@
 <script src="/assets/js/app.js"></script>
 <link rel="shortcut icon" href="/assets/imgs/svg/Imagotipo_PAWPOWER.svg" type="image/svg+xml">
 <title><?= $titulo ?? "Proyecto PAW" ?></title>
-<script type="application/ld+json">
-    {
-        "@context": "https://schema.org",
-        "@type": "Restaurant",
-        "name": "Casa de Comidas Rapidas",
-        "image": "https://www.pawpower.com/Imagotipo_PAWPOWER.svg",
-        "address": {
-            "@type": "PostalAddress",
-            "streetAddress": "Calle SiempreViva 123",
-            "addressLocality": "Ciudad CABA",
-            "addressRegion": "CABA",
-            "postalCode": "1234",
-            "addressCountry": "AR"
-        },
-        "telephone": "+54 123 456 789",
-        "servesCuisine": ["Argentina", "Porteña"],
-        "priceRange": "$$",
-        "url": "https://www.pawpower.com",
-        "aggregateRating": {
-            "@type": "AggregateRating",
-            "ratingValue": "4.5",
-            "reviewCount": "27"
-        },
-        "menu": "https://www.pawpower.com/menu",
-        "openingHours": [
-            "Mo-Su 09:00-21:00"
-        ],
-        "acceptsReservations": true
-    }
-    </script>
+
+<?php require __DIR__.'/json-ld.view.php' ?>            
---
+++ src/App/views/parts/json-ls.view.php
@@ -0,0 +1,30 @@
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Restaurant",
+        "name": "Casa de Comidas Rapidas",
+        "image": "https://www.pawpower.com/Imagotipo_PAWPOWER.svg",
+        "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Calle SiempreViva 123",
+            "addressLocality": "Ciudad CABA",
+            "addressRegion": "CABA",
+            "postalCode": "1234",
+            "addressCountry": "AR"
+        },
+        "telephone": "+54 123 456 789",
+        "servesCuisine": ["Argentina", "Porteña"],
+        "priceRange": "$$",
+        "url": "https://www.pawpower.com",
+        "aggregateRating": {
+            "@type": "AggregateRating",
+            "ratingValue": "4.5",
+            "reviewCount": "27"
+        },
+        "menu": "https://www.pawpower.com/menu",
+        "openingHours": [
+            "Mo-Su 09:00-21:00"
+        ],
+        "acceptsReservations": true
+    }
+    </script>
