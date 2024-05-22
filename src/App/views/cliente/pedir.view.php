c689de0f0b1cb07acfa78d56aa748c5e454ee78f
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-05-22 13:37:40 -0300

    acomodando estilos en view pedir

--- src/App/views/cliente/pedir.view.php
+++ src/App/views/cliente/pedir.view.php
@@ -19,43 +19,49 @@
 
         <section>
             <form action="/pedido/new" method="post" class="formulario_pedido">
+                
                 <fieldset class="container container_carrito"> <!--Fieldset Carrito-->
                     <h3 class="titulo_fieldset">Mi Carrito</h3>
                     <table class="stacked-table">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Descripción</th>
-                <th>Precio</th>
-                <th>Cantidad</th>
-                <th>Acción</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Las filas del carrito se actualizarán dinámicamente -->
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="5">Total: $30.00</td>
-            </tr>
-        </tfoot>
-    </table>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Descripción</th>
+                                <th>Precio</th>
+                                <th>Cantidad</th>
+                                <th>Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Las filas del carrito se actualizarán dinámicamente -->
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="5">Total: $30.00</td>
+                            </tr>
+                        </tfoot>
+                    </table>
                     <a href="/nuestro_menu" class="boton boton_negro btn-pedido">Agregar</a>
                 </fieldset>
 
-                <nav class="selector_fieldset">
-                    <h3 class="titulo_fieldset_delivery">
-                        <input type="checkbox" name="menuDelivery" id="menuDelivery">
-                        <label for="menuDelivery" class="menuFieldSets">Delivery</label>
-                    </h3>
-                    <h3 class="titulo_fieldset_take_away">Take Away</h3>
-                    <h3 class="titulo_fieldset_en_el_lugar">En el Lugar</h3>
-                </nav>
 
                 <fieldset class="container container_formulario container_delivery">
+
                     <label for="nombre">Ingrese su nombre</label>
                     <input type="text" name="nombre" id="nombre">
-                    <input type="text" name="tipo" id="tipo" value="Delivery" class="input_oculto">
+                    
+                    <label for="tipo">Ingrese Tipo Pedido</label>
+                    <select id="forma_pago_take_away" name="forma_pago_take_away">
+                        <option value="delivery">Delivery</option>
+                        <option value="en-el-local">Retiro por el Local</option>
+                    </select>
+
+                    <label for="numero_sucursal_take_away">Selecciona la sucursal:</label>
+                    <select id="numero_sucursal_take_away" name="numero_sucursal_take_away">
+                        <option value="Local A">Local A</option>
+                        <option value="Local B">Local B</option>
+                    </select>
+
                     <label for="direccion">Ingrese su direccion</label>
                     <input type="text" name="direccion" id="direccion">
                     <label for="observaciones">Observaciones</label>
@@ -65,67 +71,9 @@
                         <option value="mercado_pago">Mercado Pago</option>
                         <option value="efectivo">Pago en efectivo</option>
                     </select>
-                    <aside>
-                        <h4 class="titulo_resumen">Resumen</h4>
-                        <ul>
-                            <li>Producto 1 - $10</li>
-                            <li>Producto 2 - $20</li>
-                            <li>Producto 3 - $15</li>
-                            <li>Total: $45</li>
-                        </ul>
-                    </aside>
+
                 </fieldset>
 
-                <fieldset class="container container_formulario container_take_away"> <!-- .container_take_away { display: none }-->
-                    <label for="nombre_take_away">Ingrese su nombre</label>
-                    <input type="text" name="nombre_take_away" id="nombre_take_away">
-                    <label for="numero_sucursal_take_away">Selecciona la sucursal:</label>
-                    <select id="numero_sucursal_take_away" name="numero_sucursal_take_away">
-                        <option value="1">1 sucursal</option>
-                        <option value="2">2 sucursales</option>
-                        <option value="3">n sucursales</option>
-                    </select>
-                    <label for="forma_delivery">Selecciona una forma de pago:</label>
-                    <select id="forma_pago_take_away" name="forma_pago_take_away">
-                        <option value="mercado_pago">Mercado Pago</option>
-                        <option value="efectivo">Pago en efectivo</option>
-                    </select>
-                    <aside>
-                        <h4 class="titulo_resumen">Resumen</h4>
-                        <ul>
-                            <li>Producto 1 - $10</li>
-                            <li>Producto 2 - $20</li>
-                            <li>Producto 3 - $15</li>
-                            <li>Total: $45</li>
-                        </ul>
-                    </aside>
-                </fieldset>
-
-                <fieldset class="container container_formulario container_en_el_lugar">
-                    <label for="nombre_en_el_lugar">Ingrese su nombre</label>
-                    <input type="text" name="nombre_en_el_lugar" id="nombre_en_el_lugar">
-                    <label for="numero_sucursal_en_el_lugar">Selecciona la sucursal:</label>
-                    <select id="numero_sucursal_en_el_lugar" name="numero_sucursal_en_el_lugar">
-                        <option value="1">1 sucursal</option>
-                        <option value="2">2 sucursales</option>
-                        <option value="3">n sucursales</option>
-                    </select>
-                    <label for="numero_mesa">Selecciona el n&uacute;mero de mesa:</label>
-                    <select id="numero_mesa" name="numero_mesa">
-                        <option value="1">Mesa 1</option>
-                        <option value="2">Mesa 2</option>
-                        <option value="3">Mesa 3</option>
-                    </select>
-                    <aside>
-                        <h4 class="titulo_resumen">Resumen</h4>
-                        <ul>
-                            <li>Producto 1 - $10</li>
-                            <li>Producto 2 - $20</li>
-                            <li>Producto 3 - $15</li>
-                            <li>Total: $45</li>
-                        </ul>
-                    </aside>
-                </fieldset>
                 <input type="submit" value="Pedir" class="boton boton_negro  btn-pedido"> <!--ESTA ACTIVO SOLO CUANDO EL USUARIO ELIGE LA OPCION PEDIR EN EL LUGAR -->
             </form>
         </section>
