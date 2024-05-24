3002ff4d72445f8139e00fe6c821c0d7504903f5
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-05-24 17:49:47 -0300

    eliminando clases innecesarias

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -125,143 +125,38 @@ class appPAW {
                     
                             // carrito.actualizarCartel(inputCantidad.value);
                         });
-                    });                  
+                    });     
+                    
+                    /**
+                     * interacciones con el formulario
+                     */
+                    
+                    // Seleccionar el select y el campo de dirección
+                    const formaPagoSelect = document.querySelector("#tipo");
+                    const direccionInput = document.querySelector("#direccion");
+                    const direccionLabel = document.querySelector("#direccion_label");
+
+                    // Agregar un event listener al select para detectar cambios en su valor
+                    formaPagoSelect.addEventListener("change", function() {
+                        // Verificar el valor seleccionado
+                        if (this.value === "en-el-local") {
+                            // Ocultar el campo de dirección y su etiqueta
+                            direccionInput.style.display = "none";
+                            direccionLabel.style.display = "none";
+                            direccionInput.removeAttribute("required");
+                        } else {
+                            // Mostrar el campo de dirección y su etiqueta
+                            direccionInput.style.display = "block"; // O "inline-block", dependiendo del diseño deseado
+                            direccionLabel.style.display = "block"; // O "inline-block", dependiendo del diseño deseado
+                        }
+                    }); 
                 })
             }
 
-           if(['/nuestro_menu'].includes(window.location.pathname))    
-            {
-                PAW.cargarScript("Cart", "/assets/js/components/cart.js", () =>{
 
-                    const cart = new Cart();
-
-                    cart.mostrarCarritoActual();
-                    cart.updateQuantities();
-
-                    // document.querySelectorAll('.btn_increment').forEach(btn => {
-                    //     btn.addEventListener('click', function(event) {
-                    //         event.preventDefault();
-                    //         const plateId = this.getAttribute('data-id');
-                    //         cart.addToCart(plateId, 1);
-                    //         cart.mostrarCarritoActual();
-                    //     });
-                    // });
-
-                    document.querySelectorAll('.btn_decrement').forEach(btn => {
-                        btn.addEventListener('click', function(event) {
-                            event.preventDefault();
-                            const platoId = event.target.getAttribute('data-id');
-                            console.log(`decrement- platoId: ${platoId}`)
-                            cart.removeFromCart(platoId);
-                            cart.mostrarCarritoActual();
-                        })
-                    });
-
-                    // Obtener las cookies
-                    const platosCookie = cart.getCookie('platos');
-                    console.log(platosCookie);
             
-                    // Verificar si la cookie existe
-                    if (platosCookie) {
-                        // Convertir la cookie a un array de IDs de platos
-                        const platosIds = JSON.parse(platosCookie);
-                        
-                        
-                        // Realizar una solicitud fetch para obtener los detalles de los platos
-                        fetch('/plato-all-in-cart?lista_encoded=' + encodeURIComponent(platosCookie))
-                            .then(response => response.json())
-                            .then(data => {
-                                // Manejar los datos devueltos
-                                console.log(data);
-                                const table = document.querySelector('table');
-                                cart.updateCarrito(data, table);
-                                
-                                // Aquí puedes hacer lo que necesites con los datos de los platos
-                            })
-                            .catch(error => {
-                                console.error('Error en la solicitud de platos-en-carrito: ' + error);
-                            });
-                    }
-
-                    // Seleccionar el select y el campo de dirección
-                    const formaPagoSelect = document.querySelector("#tipo");
-                    const direccionInput = document.querySelector("#direccion");
-                    const direccionLabel = document.querySelector("#direccion_label");
-
-                    // Agregar un event listener al select para detectar cambios en su valor
-                    formaPagoSelect.addEventListener("change", function() {
-                        // Verificar el valor seleccionado
-                        if (this.value === "en-el-local") {
-                            // Ocultar el campo de dirección y su etiqueta
-                            direccionInput.style.display = "none";
-                            direccionLabel.style.display = "none";
-                            direccionInput.removeAttribute("required");
-                        } else {
-                            // Mostrar el campo de dirección y su etiqueta
-                            direccionInput.style.display = "block"; // O "inline-block", dependiendo del diseño deseado
-                            direccionLabel.style.display = "block"; // O "inline-block", dependiendo del diseño deseado
-                        }
-                    });     
-
-
-                });
-            }
-
-            if(['/pedir'].includes(window.location.pathname))    
-                {
-                    PAW.cargarScript("Cart", "/assets/js/components/cart.js", () => {
-                 
-                        const cart = new Cart();
-                
-                        // Obtener las cookies
-                        const platosCookie = cart.getCookie('platos');
-                        console.log(platosCookie);
-                
-                        // Verificar si la cookie existe
-                        if (platosCookie) {
-                            // Convertir la cookie a un array de IDs de platos
-                            const platosIds = JSON.parse(platosCookie);
-                            
-                            
-                            // Realizar una solicitud fetch para obtener los detalles de los platos
-                            fetch('/plato-all-in-cart?lista_encoded=' + encodeURIComponent(platosCookie))
-                                .then(response => response.json())
-                                .then(data => {
-                                    // Manejar los datos devueltos
-                                    console.log(data);
-                                    const table = document.querySelector('table');
-                                    cart.updateCarrito(data, table);
-                                    
-                                    // Aquí puedes hacer lo que necesites con los datos de los platos
-                                })
-                                .catch(error => {
-                                    console.error('Error en la solicitud de platos-en-carrito: ' + error);
-                                });
-                        }
-                    });
-
-                    // Seleccionar el select y el campo de dirección
-                    const formaPagoSelect = document.querySelector("#tipo");
-                    const direccionInput = document.querySelector("#direccion");
-                    const direccionLabel = document.querySelector("#direccion_label");
-
-                    // Agregar un event listener al select para detectar cambios en su valor
-                    formaPagoSelect.addEventListener("change", function() {
-                        // Verificar el valor seleccionado
-                        if (this.value === "en-el-local") {
-                            // Ocultar el campo de dirección y su etiqueta
-                            direccionInput.style.display = "none";
-                            direccionLabel.style.display = "none";
-                            direccionInput.removeAttribute("required");
-                        } else {
-                            // Mostrar el campo de dirección y su etiqueta
-                            direccionInput.style.display = "block"; // O "inline-block", dependiendo del diseño deseado
-                            direccionLabel.style.display = "block"; // O "inline-block", dependiendo del diseño deseado
-                        }
-                    });                    
-                }
         
-            })
+        })
     }
 }
 
