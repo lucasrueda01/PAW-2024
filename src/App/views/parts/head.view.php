ed1156c1f10495bb9d3876540ceb4dd3b3973243
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-05-28 11:52:59 -0300

    agregando tag meta content segurity policy

--- src/App/views/parts/head.view.php
+++ src/App/views/parts/head.view.php
@@ -6,6 +6,19 @@
 <script src="/assets/js/components/paw.js"></script>
 <script src="/assets/js/app.js"></script>
 <link rel="shortcut icon" href="/assets/imgs/svg/Imagotipo_PAWPOWER.svg" type="image/svg+xml">
+
+    <!-- Content-Security-Policy -->
+    <meta http-equiv="Content-Security-Policy" content="
+        default-src 'self';
+        style-src 'self';
+        img-src 'self';
+        font-src 'self';
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-ancestors 'self';
+    ">
+    
 <title><?= $titulo ?? "Proyecto PAW" ?></title>
 
 <?php require __DIR__.'/json-ld.view.php' ?>            
